e11cf9d118ae9321c3d4ac64335a087e86cda00b
Keartroth <mecarrolljr@gmail.com>
2020-02-20 14:07:03 -0600

    creating master branch for NSS Daily Journal

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const journalEntry = () => {    
+    const conceptsData = document.getElementById("concepts").value;
+    const moodData = document.getElementById("mood").value;
+    const dateData = document.getElementById("date").value;
+    const journalData = document.getElementById("journal").value;
+
+    var myData = `Concepts Covered:     ${conceptsData}     Current Mood:   ${moodData}     Date:   ${dateData}    Daily Journal Entry: ${journalData}`;
+    window.open(`mailto:mecarrolljr@tutanota.com?subject=NSS Daily Journal: ${dateData}&body=${myData}`);
+}
+
+// This is sample; do not edit or delete!!
+// $(".btn.btn-primary.form-submit-me").click(function() {
+//     var myData = "Email:  " + $("#exampleFormControlInput1").val() + "    " + "Rating:  " + $("#exampleFormControlSelect1").val() + "    " + "Additional Feedback:  " + $("#exampleFormControlTextarea2").val();
+//     window.open('mailto:mecarrolljr@tutanota.com?subject=Michael Carroll Website Review&body=' + myData);
+// })
