dea2e5522e411c7417edee05eb0dbd362beb7857
Keartroth <mecarrolljr@gmail.com>
2020-02-28 09:05:18 -0600

    added event listener to journal entry button and connected it to the journal entry function which submits data to email.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import { useJournalEntries } from "./journalDataProvider.js";
+
 const journalEntry = () => {    
     const conceptsData = document.getElementById("concepts").value;
     const moodData = document.getElementById("mood").value;
@@ -8,8 +10,10 @@ const journalEntry = () => {
     window.open(`mailto:mecarrolljr@tutanota.com?subject=NSS Daily Journal: ${dateData}   Current Mood:   ${moodData}&body=${myData}`);
 }
 
-// This is sample; do not edit or delete!!
-// $(".btn.btn-primary.form-submit-me").click(function() {
-//     var myData = "Email:  " + $("#exampleFormControlInput1").val() + "    " + "Rating:  " + $("#exampleFormControlSelect1").val() + "    " + "Additional Feedback:  " + $("#exampleFormControlTextarea2").val();
-//     window.open('mailto:mecarrolljr@tutanota.com?subject=Michael Carroll Website Review&body=' + myData);
-// })
+const initializeLogButtonEvent = () => {
+    const button = document.querySelector("#journalButton");
+    button.addEventListener("click",journalEntry);
+};
+
+
+initializeLogButtonEvent();
